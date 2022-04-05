cd26d0a74dedab25a02b4828cb50eba21a49e22f
WeiiswurstDev <arved.ehrnsperger@gmail.com>
2022-04-06 01:45:46 +0200

    Add Github token to actions

---
+++ src/main/java/dev/weiiswurst/placestom/util/UpdateChecker.java
@@ -0,0 +1,16 @@
+package dev.weiiswurst.placestom.util;
+
+import org.jetbrains.annotations.ApiStatus;
+
+public class UpdateChecker {
+
+    private UpdateChecker() {
+    }
+
+    @ApiStatus.Internal
+    public static void checkForUpdates() {
+        
+    }
+
+
+}
