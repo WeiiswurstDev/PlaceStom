b6d6b5a977238d63cbd8ba4bd3fad4ed7a4e2576
WeiiswurstDev <arved.ehrnsperger@gmail.com>
2022-04-06 00:12:05 +0200

    Fix Teleport command

--- src/main/java/dev/weiiswurst/placestom/commands/TeleportCommand.java
+++ src/main/java/dev/weiiswurst/placestom/commands/TeleportCommand.java
@@ -11,15 +11,17 @@ import net.minestom.server.entity.Player;
 
 public class TeleportCommand extends Command {
 
-    private static final int MAX_VALUE = Integer.getInteger("placestom.worldborder-size");
+    private static final int MAX_VALUE = Integer.getInteger("placestom.worldborder-size") / 2 - 1;
 
     public TeleportCommand() {
         super("teleport", "tp");
-        ArgumentNumber<Integer> x = ArgumentType.Integer("x").max(MAX_VALUE).min(-MAX_VALUE);
-        ArgumentNumber<Integer> z = ArgumentType.Integer("z").max(MAX_VALUE).min(-MAX_VALUE);
+        ArgumentNumber<Integer> xArg = ArgumentType.Integer("x").max(MAX_VALUE).min(-MAX_VALUE);
+        ArgumentNumber<Integer> zArg = ArgumentType.Integer("z").max(MAX_VALUE).min(-MAX_VALUE);
         setCondition(Conditions::playerOnly);
         CommandExecutor commandExecutor = (sender, context) ->
-                ((Player)sender).teleport(new Pos(context.get("x"), 40.1, context.get("z")));
-        addSyntax(commandExecutor, x, z);
+            ((Player)sender).teleport(new Pos(context.get(xArg).doubleValue(), 40.1, context.get(zArg).doubleValue()));
+
+
+        addSyntax(commandExecutor, xArg, zArg);
     }
 }
